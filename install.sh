4788f737106ab259241a1195403e0b1e7956ac9a
Peter Pham <peterpham86@gmail.com>
2022-06-17 11:49:33 -0700

    fix spelling

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo cp bluetooh_kill.sh /Users/Shared/
-sudo cp com.bluetoothkill.hackintosh.plist /Library/LaunchDaemons/com.bluetoohkill.hackintosh.plist
-sudo launchctl load /Library/LaunchDaemons/com.bluetoohkill.hackintosh.plist
+sudo cp bluetooth_kill.sh /Users/Shared/
+sudo cp com.bluetoothkill.hackintosh.plist /Library/LaunchDaemons/com.bluetoothkill.hackintosh.plist
+sudo launchctl load /Library/LaunchDaemons/com.bluetoothkill.hackintosh.plist
 sudo launchctl start com.bluetoothkill.hackintosh.plist

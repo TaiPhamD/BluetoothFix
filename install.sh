eb560535875c71c13b9744ce3634ad9f4d717046
ray <henrayluo@gmail.com>
2022-08-25 23:56:52 +0800

    Add permission to shell script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo cp bluetooth_kill.sh /Users/Shared/
+sudo cp bluetooth_kill.sh /Users/Shared/ && sudo chmod 755 /Users/Shared/bluetooth_kill.sh
 sudo cp com.bluetoothkill.hackintosh.plist /Library/LaunchDaemons/com.bluetoothkill.hackintosh.plist
 sudo launchctl load /Library/LaunchDaemons/com.bluetoothkill.hackintosh.plist
 sudo launchctl start com.bluetoothkill.hackintosh.plist
